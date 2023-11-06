33b2934feeb3ec3aacb50321a61e6b20508575b3
Hmdy <hamdy@dafater.sa>
2023-11-06 21:27:17 +0200

    Change debug mode

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q*lo)3q6=d8wb)=1u$evx*7x^!f2c)eg6v7+trdw^l@l#g8k-9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
